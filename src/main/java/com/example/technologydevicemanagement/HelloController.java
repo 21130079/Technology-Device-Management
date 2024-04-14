e373a85b905a16c561dfbfb6e4578afcc1d4e6c6
= <=>
2024-04-14 18:04:32 +0700

    add UI Login and OrderDetails

---
+++ src/main/java/com/example/technologydevicemanagement/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.technologydevicemanagement;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
