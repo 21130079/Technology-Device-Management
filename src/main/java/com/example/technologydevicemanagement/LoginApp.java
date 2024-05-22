7981bd1786e53e9aea0dfd2594b73f886e7f86b0
= <=>
2024-05-22 23:43:32 +0700

    update UI ogin

--- src/main/java/com/example/technologydevicemanagement/LoginApp.java
+++ src/main/java/com/example/technologydevicemanagement/LoginApp.java
@@ -4,9 +4,14 @@ import com.example.technologydevicemanagement.controller.LoginController;
 import com.example.technologydevicemanagement.util.DBUtil;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
+import java.awt.*;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class LoginApp extends Application {
@@ -16,6 +21,8 @@ public class LoginApp extends Application {
     public void start(Stage stage) throws IOException {
         DBUtil.doConnection();
         FXMLLoader fxmlLoader = new FXMLLoader(LoginApp.class.getResource("view/login.fxml"));
+
+        stage.setTitle("Technology Equipment Sales Management System");
 
         scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.setTitle("Sale Management");
